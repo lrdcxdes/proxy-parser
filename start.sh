6f59753095cd1c098c9e2b3d04a6429f683388f3
LORD_CODE <83734728+LORD-ME-CODE@users.noreply.github.com>
2021-12-29 22:17:47 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "   *** Script made by lordcodes"
 apt-get install python3-lxml
-pip3 install beautifulsoup4 requests rich tqdm
+pip3 install beautifulsoup4 requests rich tqdm lxml
 python3 run.py 
