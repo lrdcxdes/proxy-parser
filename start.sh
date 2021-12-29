b505be5190cac124de9f3c661f0a7c02650812bf
LORD_CODE <83734728+LORD-ME-CODE@users.noreply.github.com>
2021-12-29 22:09:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "   *** Script made by lordcodes"
-python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt --no-index --find-links file:///tmp/packages
 python run.py
